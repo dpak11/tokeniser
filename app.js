1d58497ebd9a83f11d8cf0056031735347a779a0
deepak <deepakb@protonmail.com>
2020-03-13 11:27:05 +0530

    decode token and verify hash

--- app.js
+++ app.js
@@ -1,96 +1,15 @@
-const { SHA256 } = require('crypto-js');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const {tokeniser} = require('./tokeniser');
 const app = express();
-
 app.use(cookieParser());
 
-
-function encodeCookieData(data, hKey) {
-    let base64Data = Buffer.from(JSON.stringify(data), 'binary').toString('base64');
-    let base64Hkey = Buffer.from(JSON.stringify(hKey), 'binary').toString('base64');
-
-    //base64data = '65954jgndfkgjd95879354j43'
-    //hashedKey = '4jndis74/hrerwfdhfs764324iuhrnfdsnfs'
-    const strSequence = "qwertyuio/+=1234567890plkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
-    let hSet = new Set([...base64Hkey]);
-    let hashUniqueSequence = [...hSet].join("");
-    console.log("hKey: " + hKey);
-    console.log("base64Hkey: "+base64Hkey);
-    console.log("hashUniqueSequence: " + hashUniqueSequence);
-    console.log("Base64: " + base64Data);
-    // Example:
-    // if hashUniqueSequence = "3grjey48f/10vu"
-    // strSequence    =        "qwertyuio/+=1234567890plkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
-    // all occurence of '3' in base64Data will be replace by 'q'.
-    // Similarly 'g' relaces 'w', 'r' replaces 'e' and so on...
-    for (let i = 0; i < hashUniqueSequence.length; i++) {
-        let regex = new RegExp(hashUniqueSequence[i], "g");
-        base64Data = base64Data.replace(regex, strSequence[i]);
-    }
-    console.log("Base64: " + base64Data);
-    return base64Data;
-
-}
-
-function hashCookieToken(data, key) {
-    let cookieString = JSON.stringify({
-        cookie_data: data,
-        key_cookie: key
-    });
-    let base64 = Buffer.from(cookieString, 'binary').toString('base64')
-    return SHA256(base64).toString();
-}
-
-
-function generateToken({cookieName, cookieData, secretKey, expiresIn, response=null }) {
-    //let cookie = req.cookies[cookieName];
-    //can be 10s,10m,2h,4d
-    if ((/^[0-9]{1,3}[smhd]{1}$/).test(expiresIn)) {
-        let lastStr = expiresIn.slice(expiresIn.length - 1);
-        let expireNum = Number(expiresIn.slice(0, expiresIn.length - 1));
-        let maxCookieAge = 0;
-        if (lastStr == "s") { maxCookieAge = 1000 * expireNum }
-        if (lastStr == "m") { maxCookieAge = 1000 * expireNum * 60 }
-        if (lastStr == "h") { maxCookieAge = 1000 * expireNum * 3600 }
-        if (lastStr == "d") { maxCookieAge = 1000 * expireNum * 3600 * 24 }
-        let hashed = hashCookieToken(cookieData, SHA256(secretKey).toString());
-        let encodedData = encodeCookieData(cookieData, SHA256(secretKey).toString());
-        let encodeHashed = {
-            data: encodedData,
-            token: hashed
-        };
-        if(response){
-            response.cookie(cookieName, encodeHashed, { maxAge: maxCookieAge });
-        }
-        
-        return {
-            error:false,
-            status:"ok",
-            cookie:encodeHashed
-        }
-    }
-    return {
-        error:true,
-        status:"Invalid expiration period for cookie",
-        cookie:null
-    }
-
-}
-
-function getCookieToken(req, cookieName) {
-    let cookie = req.cookies[cookieName];
-
-}
-
-
-
 app.get('/setuser', (req, res) => {
-    const mycookie = generateToken({
-        cookieName: "superCookie",
-        cookieData: { name: "deeps", role: "admin" },
+    const mycookie = tokeniser.setToken({
+        cookieName: "supercookie",
+        cookieData: { name: "aaa", role: "none" },
         secretKey: "simplePassword",
-        expiresIn: "2m",
+        expiresIn: "5m",
         response: res
     });
     if(mycookie.error){
@@ -102,10 +21,13 @@ app.get('/setuser', (req, res) => {
 
 //Iterate users data from cookie 
 app.get('/getuser', (req, res) => {
-    //shows all the cookies 
-    res.send(req.cookies);
-    console.log(req.cookies);
-    console.log(req.cookies.userData);
+    //shows all the cookies
+    const mycookie = tokeniser.getToken({
+        cookieName: "supercookie",
+        secretKey: "simplePassword",
+        request: req
+    });
+    res.send(mycookie);
 });
 
 
---
+++ tokeniser.js
@@ -0,0 +1,133 @@
+const { SHA256 } = require('crypto-js');
+const strSequence = "poiuytrewq+/=asdfghjklmnbv0123456789cxzQWERTYUIOPLKJHGFDSAZXCVBNM";
+
+
+function encodeCookieData(data, hKey) {   
+    
+    //qwertyuiop
+    //poiuytrewq
+    // Example:
+    // if uniqueSequence =     "3grj7ey48f/10vu"
+    // reverseUnique    =      "uv01/f84ye7jrg3";
+    // base64data     =        "7gywesadasew423uhd"
+    // loop through base64data, replace all occurences of '7 in base64Data by '/_'(from unique sequence corresponds 7 in reverse unique).
+    // Similarly 'g' relaced by 'v_', 'y' replaces '8_' and so on...
+    let uniqueSequence = getUniqueKey(hKey);
+    let b64Data = Buffer.from(JSON.stringify(data), 'binary').toString('base64');
+    let base64Data = b64Data.split("").reverse();
+    let alteredBase64 = [];
+    base64Data.forEach((base) => {        
+        let index = uniqueSequence.reversed.indexOf(base);
+        alteredBase64.push(`${uniqueSequence.unique[index]}_`)
+    });    
+    return Buffer.from(alteredBase64.join(""), 'binary').toString('base64')
+}
+
+function decodeCookieData(data, hKey) {
+    
+    let uniqueSequence = getUniqueKey(hKey);
+    let _data = Buffer.from(data, "base64").toString('ascii');
+    let d = _data.split("_");
+    let altered = [];
+    d.forEach((val) => {        
+        let index = uniqueSequence.unique.indexOf(val);
+        altered.push(uniqueSequence.reversed[index])
+    }); 
+    
+    let base64 = altered.reverse().join("");
+    base64 = Buffer.from(altered.join(""), 'base64').toString('binary');
+    return JSON.parse(base64);
+
+}
+
+function hashCookieToken(data, key) {
+    let cookieString = JSON.stringify({
+        cookie_data: data,
+        secure: key
+    });
+    let base64 = Buffer.from(cookieString, 'binary').toString('base64')
+    return SHA256(base64).toString();
+}
+
+function getUniqueKey(hKey){
+    let base64Hkey = Buffer.from(JSON.stringify(hKey), 'binary').toString('base64');
+    let hSet = new Set([...base64Hkey,...strSequence]);
+    let uniqueSequence = [...hSet];
+    let reverseUnique = [...hSet].reverse();
+    return {unique:uniqueSequence, reversed:reverseUnique}
+
+}
+
+
+function setToken({ cookieName, cookieData, secretKey, expiresIn, response = null }) {
+    
+    //expiresIn examples: 10s,10m,2h,4d
+    if (typeof cookieName != "string") {
+        return {
+            error: true,
+            status: "Invalid CookieName",
+            cookie: null
+        }
+    }
+    if ((/^[0-9]{1,3}[smhd]{1}$/).test(expiresIn)) {
+        let lastStr = expiresIn.slice(expiresIn.length - 1);
+        let expireNum = Number(expiresIn.slice(0, expiresIn.length - 1));
+        let maxCookieAge = 0;
+        if (lastStr == "s") { maxCookieAge = 1000 * expireNum }
+        if (lastStr == "m") { maxCookieAge = 1000 * expireNum * 60 }
+        if (lastStr == "h") { maxCookieAge = 1000 * expireNum * 3600 }
+        if (lastStr == "d") { maxCookieAge = 1000 * expireNum * 3600 * 24 }
+        let hashed = hashCookieToken(cookieData, SHA256(secretKey).toString());
+        let encodedData = encodeCookieData(cookieData, SHA256(secretKey).toString());
+        let encodeHashed = {
+            data: encodedData,
+            sign: hashed
+        };
+        if (response) {
+            response.cookie(cookieName, encodeHashed, { maxAge: maxCookieAge });
+        }
+
+        return {
+            error: false,
+            status: "ok",
+            cookie: encodeHashed
+        }
+    }
+    return {
+        error: true,
+        status: "You have set an invalid expiration period.",
+        cookie: null
+    }
+
+}
+
+function getToken({ cookieName, secretKey, request }) {
+    if (typeof cookie == "undefined") {
+        return {
+            error: true,
+            status: "Cookie not found",
+            data: null
+        }
+    }
+    let decodedData = decodeCookieData(cookie.data, SHA256(secretKey).toString());
+    let hashed = hashCookieToken(decodedData, SHA256(secretKey).toString());
+    if (hashed == cookie.sign) {
+        return {
+            error: false,
+            status: "ok",
+            data: decodedData
+        }
+    }
+    return {
+        error: true,
+        status: "Cookie has been tampered"
+    }
+}
+
+
+module.exports = {
+    tokeniser: {
+        setToken,
+        getToken
+    }
+}
